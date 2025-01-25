ca53d7b10b9a8ae20687d861d305272b70648365
Leandro Bonora <60291393+LBonora@users.noreply.github.com>
2025-01-25 22:25:48 +0000

    Adds `getXrefTable` prototype

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 import { Buffer } from "node:buffer";
 
-export function getStartXref(fileName, fileSize = null, buffer = 64) {
+export function getStartxref(fileName, fileSize = null, buffer = 64) {
   if (fileSize === null) {
     fileSize = fs.statSync(fileName).size;
   }
@@ -18,4 +18,30 @@ export function getStartXref(fileName, fileSize = null, buffer = 64) {
   });
 }
 
-//const startxref = await getStartXref(fileName);
+export function getXrefTable(fileName, startxref) {
+  return new Promise((resolve, reject) => {
+    const stream = fs.createReadStream(fileName, { start: startxref });
+    stream.on("error", (error) => {
+      reject(error);
+    });
+    stream.on("data", (data) => {
+      const result = [];
+      let curPos = 1 + data.indexOf(0x0a);
+      let lastPos = 0;
+      while (curPos > 0) {
+        const foo = data.slice(lastPos, curPos).toString();
+        if (foo.includes(new Buffer.from("trailer"))) {
+          break;
+        }
+        result.push({
+          start: foo.slice(0, 10),
+          gen: foo.slice(11, 16),
+          sts: foo[17] || null,
+        });
+        lastPos = curPos;
+        curPos = 1 + data.indexOf(0x0a, curPos);
+      }
+      resolve(result);
+    });
+  });
+}
